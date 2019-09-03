62a106be36c7fcafd2bce78447c608c7f4fa383a
yocuervoc <yocuervoc@unal.edu.co>
2019-09-02 23:13:43 -0500

    database model users

--- model/user.js
+++ model/user.js
@@ -6,6 +6,28 @@ module.exports = function  setupUserModel (config){
     const sequelize = setupDatabase (config)
 
     return sequelize.define('user', {
-        
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        lastName: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        username: {
+            type: Sequelize.STRING,
+            allowNull: false
+        },
+        password: {
+            type: Sequelize.STRING,
+            allowNull: false
+        }
+
     })
 }
